9d1b92d3eac52abc987a40650f281a105108ea71
Palvett Blaise <blaisepalvett@gmail.com>
2026-06-20 22:21:48 +0100

    chore: format JavaScript with standard linter

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
 
 async function checkWeather () {
   const response = await fetch(apiUrl + `&appid=${apiKey}`);
-  var data = await response.json();
+  const data = await response.json();
 
   console.log(data);
 }
 
 checkWeather();
+
