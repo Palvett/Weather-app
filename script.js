5edece799324a1cb6755b5003637b7555708c234
Palvett Blaise <blaisepalvett@gmail.com>
2026-07-07 15:15:26 +0100

    project continution

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Functionality
 
-const apiKey = "deda01dac68f7c8399cbc11da6ec3711";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = 'deda01dac68f7c8399cbc11da6ec3711'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
 const cityName = document.querySelector('.city')
 const temperature = document.querySelector('.temp')
@@ -11,12 +11,10 @@ const searchBtn = document.querySelector('.search-btn')
 const cityInput = document.querySelector('.city-input')
 
 async function checkWeather(city) {
-  const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
-  const data = await response.json();
-  console.log(data);
+  const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`)
+  const data = await response.json()
+  console.log(data)
 }
 
-checkWeather("yaounde");
+checkWeather("yaounde")
 
-
-// const weatherform = document.querySelector(weatherform);
